a224d807e433ca35ee3ed1e7cdcfa8a7a7ef27f5
desak29 <desak29@icloud.com>
2016-04-06 09:19:21 -0400

    ~modified background wrapper

--- js/controller.js
+++ js/controller.js
@@ -8,11 +8,12 @@ tweetApp.controller('mainController', function($scope, $http, $routeParams, $int
 	// $scope.imagePath =
 
 	$http.get(url).success(function(data){
-		$scope.data = data.statuses;
+		$scope.data = data.statuses;console.log(data)
 		for(i=0; i<$scope.data.length; i++){
 			var time = $scope.data[i].created_at;
 			var tweetTime = new Date(time);
 			$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+
 
 			$interval(function(){
 				for(i=0; i<$scope.data.length; i++){
